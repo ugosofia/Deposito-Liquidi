f607b8878472a73dba0a400e7d4694c9620737e8
ugosofia <ugo.sofia97@gmail.com>
2022-03-26 16:59:21 +0100

    Modifiche al main.

--- Contenitore/TestClass.java
+++ Contenitore/TestClass.java
@@ -6,7 +6,14 @@ public class TestClass {
         Contenitore c1 = new Bidoni(1, Contenitore.livelloTossico.ALTO, "Vino", 50);
         Contenitore c2 = new Bidoni(2, Contenitore.livelloTossico.NULLO, "Acqua", 60);
 
+
+        System.out.println(c1.confronta(c2));
+        System.out.println();
+
         Contenitore c3 = new Lattine(3, Contenitore.livelloTossico.MEDIO, "CocaCola", 20);
         Contenitore c4 = new Lattine(4, Contenitore.livelloTossico.BASSO, "Aranciata", 20);
+
+        System.out.println(c3.confronta(c4));
+        System.out.println();
     }
 }
